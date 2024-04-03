c5afcb0d5ee8c14aba3954ebbb434611d5b8dad8
Aniruddha Bagal <bagalaniruddha@gmail.com>
2024-04-03 21:39:09 +0530

    File Added

---
+++ src/main/java/model/UserRoles.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class UserRoles {
+    private int id;
+    private String roleName;
+    private String roleDescription;
+    private int accessLevel;
+    private Timestamp creationDate;
+    private Timestamp lastModifiedDate;
+    private int userRoleId;
+
+    public UserRoles() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
+
+    public int getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(int accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Timestamp getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Timestamp lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public int getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(int userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+}
